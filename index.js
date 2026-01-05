2029608df01fbf7de1c6f18b31e90c98c0c49af0
FotsaKelly <fotsaamanda@gmail.com>
2026-01-05 17:09:12 +0100

    Update index.js and add index.html

--- index.js
+++ index.js
@@ -1,5 +1,16 @@
-function hello() {
-    console.log("Hello, world!");
+
+let count = 0;
+document.getElementById('result').innerText = count
+document.getElementById('input-value').addEventListener('input', (event) => {
+    count = event.target.value;
+  
+});
+
+const increment =()=> {
+    count = parseInt(count) + 1;
+    document.getElementById('result').innerText = count;
 }
 
-hello();
+document.getElementById('increment-button').addEventListener('click', increment);
+
+;
